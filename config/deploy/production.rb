090f072fc1bfba9c4b5ec512f32d6b13d3c99cfe
Alya <alya@MacBook-Air-Alya.local>
2017-12-25 17:41:07 +0300

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,10 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+set :branch, 'master'
+set :rails_env, 'production'
+set :puma_env, 'production'
+
 server "92.53.91.95", user: "deployer", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
