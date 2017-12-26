54fd9cd744c5c5cc447712dd82bebaf221e56172
Alya <alya@MacBook-Air-Alya.local>
2017-12-27 01:06:57 +0300

    deploy 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.7.2'
 set :application, 'pushkin-bot'
 set :repo_url, 'git@github.com:AlyaTorshina/pushkin-contest-bot.git'
 set :deploy_to, '/var/www/pushkin/'
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/secrets.yml.key}
 set :linked_dirs, %w{log tmp/pids public/assets tmp/cache tmp/sockets vendor/bundle}
 set :ssh_options, { :forward_agent => true }
 set :pry, false
